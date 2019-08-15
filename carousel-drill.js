603a4c610e4515d423618cf183b7a9cfe93ab527
Phoebe Law <phoebebasilio@gmail.com>
2019-08-15 12:08:35 -0700

    Cat Carousel Complete

--- carousel-drill.js
+++ carousel-drill.js
@@ -1,32 +1,13 @@
-$('.thumbnail').on('click', e => {
-  console.log(e.target);
-});
+'use strict';
 
-
-
-//when you click or keyboard select thumbnail it should updated the carosel and the alt
-
-// use toggle from lightbulbs to turn on and off with aria
-// an
-
-function handleCatClicks () {
-  $('.thumbnail').on('click', event => {
-    // save reference to this bulb,
-    const targetCat = $(event.currentTarget);
-    // and reference to all other bulbs
-    const otherCats = $('.thumbnail').not(targetCat);
-    // store whether this bulb is aria-pressed.
-    // because attr() returns strings, we convert it
-    // back to a boolean by making a comparison.
-    const pressedBool = $(targetBulb).attr('aria-pressed') === 'true';
+function handleThumbnailClicks() {
+  $('.thumbnail').on('click', function(event) {
+     const imgSrc = $(this).find('img').attr('src');
+     const imgAlt = $(this).find('img').attr('alt');
     
-    // Remove 'bulb-on' and uncheck bulbs that are not this one
-    otherBulbs.removeClass('bulb-on').attr('aria-pressed', false);
-    // toggle the presence of 'bulb-on' on this bulb; 
-    // toggle aria-pressed by inverting pressedBool 
-    // using the ! operator.
-    targetBulb.toggleClass('bulb-on').attr('aria-pressed', !pressedBool);
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+    
   });
 }
 
-$(handleBulbClicks);
+$(handleThumbnailClicks);
